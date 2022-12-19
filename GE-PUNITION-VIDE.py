aab4ecabdc708acc782a978c1f95b9f62753303a
Alexis <63512507+Aldbg74@users.noreply.github.com>
2022-12-19 15:12:35 +0100

    Add files via upload

---
+++ GE-PUNITION-VIDE.py
@@ -0,0 +1,57 @@
+# GE-PUNITION
+#GEPUNITION est une nouvelle vertion de GEMOT
+#Cette vertion est axée sur la gestion de punition et de devoir
+#Elle est plus réservé a un usage de la vie-scolaire
+
+# Commande pour importer la fonction random et pour permettre un
+#choix aléatoire entre les 10 punition
+import random
+import time
+devoirs = random.randint(1, 10)
+
+#On lance le programe
+while True :
+    print("G E P U N I T I O N")
+    print("")
+    print("Génération de la punition")
+    time.sleep(5)
+
+    if devoirs == 1 :
+        print("")
+        break
+
+    if devoirs == 2:
+        print("")
+        break
+
+    if devoirs == 3:
+        print("")
+        break
+
+    if devoirs == 4:
+        print("")
+        break
+
+    if devoirs == 5:
+        print("")
+        break
+
+    if devoirs == 6:
+        print("")
+        break
+
+    if devoirs == 7:
+        print("")
+        break
+
+    if devoirs == 8:
+        print("")
+        break
+
+    if devoirs == 9:
+        print("")
+        break
+
+    if devoirs == 10 :
+        print("")
+        break
